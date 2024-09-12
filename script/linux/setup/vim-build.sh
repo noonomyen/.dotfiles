07d3888efda05c3399ba4db6d97ac413413e14a6
noonomyen <nomyen@noonomyen.com>
2024-09-12 23:40:54 +0700

    update vim build script

--- script/linux/setup/vim-build.sh
+++ script/linux/setup/vim-build.sh
@@ -1,34 +1,46 @@
 #!/usr/bin/bash
 
-sudo mkdir /opt/vim
+set -e
+
+sudo mkdir -p /opt/vim
 cd /opt/vim
 
 sudo apt remove -y vim vim-runtime gvim vim-tiny vim-common vim-gui-common
-sudo apt-get install -y libncurses5-dev libgtk2.0-dev libatk1.0-dev \
-                        libcairo2-dev libx11-dev libxpm-dev libxt-dev ruby-dev
+# libgtk2.0-dev <-> libgtk-3-dev
+sudo apt-get install -y libncurses5-dev libgtk-3-dev libatk1.0-dev \
+                        libcairo2-dev libx11-dev libxpm-dev libxt-dev ruby-dev \
+                        lua5.4-dev python3.12-dev python2.7-dev libperl-dev tcl-dev
 
-sudo wget https://github.com/vim/vim/archive/refs/tags/v9.1.0404.tar.gz -O v9.1.0404.tar.gz
+sudo wget https://github.com/vim/vim/archive/refs/tags/v9.1.0727.tar.gz -O v9.1.0727.tar.gz
 
-sudo tar -xf v9.1.0404.tar.gz
-cd vim-9.1.0404
+sudo tar -xf v9.1.0727.tar.gz
+cd vim-9.1.0727
 
-sudo ./configure --enable-cscope \
-            --enable-fontset \
-            --enable-luainterp \
-            --enable-multibyte \
-            --enable-mzschemeinterp \
-            --enable-option-checking \
-            --enable-perlinterp \
-            --enable-python3interp \
-            --enable-pythoninterp \
-            --enable-rubyinterp \
-            --enable-tclinterp \
-            --enable-terminal \
-            --enable-gui=gtk2 \
-            --enable-gpm \
-            --with-python3-config-dir=/usr/lib/python3.11/config-3.11-x86_64-linux-gnu \
-            --with-compiledby=noonomyen \
-            --prefix=/opt/vim
+sudo bash ./configure \
+    --enable-option-checking \
+    --enable-luainterp=yes \
+    --enable-mzschemeinterp \
+    --enable-perlinterp=yes \
+    --enable-python3interp=yes \
+    --enable-pythoninterp=yes \
+    --enable-tclinterp=yes \
+    --enable-rubyinterp=yes \
+    --enable-cscope \
+    --enable-terminal \
+    --enable-multibyte \
+    --enable-xim \
+    --enable-fontset \
+    --enable-gui=gtk3 \
+    --with-luajit \
+    --with-python-command=python2.7 \
+    --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+    --with-python3-command=python3.12 \
+    --with-python3-stable-abi=3.12 \
+    --with-python3-config-dir=/usr/lib/python3.12/config-3.12-x86_64-linux-gnu \
+    --with-features=huge \
+    --with-compiledby=noonomyen \
+    --prefix=/opt/vim \
+    --enable-fail-if-missing
 
 sudo make -j $(nproc)
 sudo make install
