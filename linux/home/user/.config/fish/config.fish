cf3aaf07aafcd2ab29a1adf59c843d3db54ab942
noonomyen <nomyen@noonomyen.com>
2025-12-09 18:37:27 +0700

    add args hyperlink for eza and add abbr '-' for 'cd -'

--- linux/home/user/.config/fish/config.fish
+++ linux/home/user/.config/fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
+    bind \cH backward-kill-path-component
+
     # No greeting
     set fish_greeting
 
@@ -10,7 +12,9 @@ if status is-interactive
 
     alias clear "printf '\033[2J\033[3J\033[1;1H'"
     alias cd="z"
-    alias l="eza -alghHiMnOSmU --group-directories-first --git --icons --time-style=long-iso --color=always"
-    alias ls="eza --group-directories-first --git --icons --time-style=long-iso --color=always"
+    alias l="eza -alghHiMnOSmU --group-directories-first --git --icons --time-style=long-iso --color=always --hyperlink"
+    alias ls="eza --group-directories-first --git --icons --time-style=long-iso --color=always --hyperlink"
     alias zed="zeditor"
+
+    abbr -a -- - "cd -"
 end
