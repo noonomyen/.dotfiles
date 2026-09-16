6e7e9eab0743037e07838ac19b329585eac31929
noonomyen <nomyen@noonomyen.com>
2026-09-17 01:12:38 +0700

    fix: remove redundant fzf.fish shim conflicting with plugin

--- .config/fish/conf.d/fzf.fish
+++
@@ -1,7 +0,0 @@
-# Activate fzf.fish key bindings (patrickf1/fzf.fish plugin)
-# Enables: Ctrl+R (history), Ctrl+Alt+F (files), Ctrl+Alt+L (git log),
-#          Ctrl+Alt+S (git status), Ctrl+V (variables)
-if status is-interactive
-    and functions -q fzf_configure_bindings
-    fzf_configure_bindings
-end
